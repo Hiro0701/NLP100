446e93e0f46c37ed24f93c8c9be7d594de026929
[Github <dklee1112394@gmail.com>
2024-01-23 09:31:53 +0900

    ~ A49

---
+++ C4/Chapter4.py
@@ -0,0 +1,182 @@
+neko_txt_path = 'neko.txt'
+
+# A30 形態素解析結果の読み込み
+import MeCab
+
+
+def A30(txt_path: str) -> list:
+    with open(txt_path, 'r') as file:
+        pos_list = []
+        tagger = MeCab.Tagger()
+
+        for line in file:
+            lined_list = []
+            parsed = tagger.parse(line)
+            if len(parsed) == 4:
+                continue
+
+            for parse in parsed.split('\n'):
+                splitted_pos = parse.split('\t')
+                if len(splitted_pos) <= 1:
+                    continue
+
+                lined_list.append({'表層形': splitted_pos[0],
+                                   '基本形': splitted_pos[3],
+                                   '品詞': splitted_pos[4].split('-')[0],
+                                   '品詞細分類1': splitted_pos[4].split('-')[1] if len(
+                                       splitted_pos[4].split('-')) != 1 else ''
+                                   })
+
+            pos_list.append(lined_list)
+
+        return pos_list
+
+
+# A31 動詞
+def A31(pos_list: list) -> list:
+    verb_list = []
+    for sentence in pos_list:
+        for word in sentence:
+            if word['品詞'] == '動詞':
+                verb_list.append(word['表層形'])
+
+    return verb_list
+
+
+# A32 動詞の基本形
+def A32(pos_list: list) -> list:
+    verb_list = []
+    for sentence in pos_list:
+        for word in sentence:
+            if word['品詞'] == '動詞':
+                verb_list.append(word['基本形'])
+
+    return verb_list
+
+
+# A33 「AのB」
+def A33(pos_list: list) -> list:
+    noun_list = []
+    for sentence in pos_list:
+        for i in range(len(sentence) - 2):
+            if sentence[i]['品詞'] == '名詞' and \
+                    sentence[i + 2]['品詞'] == '名詞' and \
+                    sentence[i + 1]['表層形'] == 'の':
+                noun_list.append(sentence[i]['表層形'] + sentence[i + 1]['表層形'] + sentence[i + 2]['表層形'])
+
+    return noun_list
+
+
+# A34 名詞の連接
+# import re
+
+
+def A34(pos_list: list) -> str:
+    noun_list = []
+    for sentence in pos_list:
+        current_noun = ''
+        noun_longest_list = []
+        last_word = sentence[-1]
+
+        for word in sentence:
+            if word['品詞'] == '名詞':
+                noun_check = True
+                current_noun = current_noun + word['表層形']
+                if word == last_word:
+                    noun_longest_list.append(current_noun)
+            else:
+                noun_check = False
+                noun_longest_list.append(current_noun)
+                current_noun = ''
+        noun_list.append(max(noun_longest_list, key=len))
+
+    # noun_jp_list = [s for s in noun_list if not re.search(r'[a-zA-Z]', s)]
+
+    return max(noun_list, key=len)
+
+
+# A35 単語の出現頻度
+# form='表層形' or '表層形', pos=None(全部出力) or '名詞' or '動詞', ...
+def A35(pos_list: list, pos=None, form='表層形') -> dict:
+    frequent_dict = {}
+    for sentence in pos_list:
+        for word in sentence:
+            if not pos or word['品詞'] == pos:
+                frequent_dict[word[form]] = frequent_dict.get(word[form], 0) + 1
+
+    return dict(sorted(frequent_dict.items(), key=lambda item: item[1], reverse=True))
+
+
+# A36 頻度上位10語
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rcParams['font.family'] = 'Arial Unicode MS'
+
+
+def A36(sorted_frequent_dict: dict, n=10):
+    top_n_list = list(sorted_frequent_dict.items())[:n]
+    plt.bar([i[0] for i in top_n_list], [i[1] for i in top_n_list])
+    plt.title(f'A36: Top {n} frequent words')
+    plt.xlabel('Word')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+# A37 「猫」と共起頻度の高い上位10語
+def A37(pos_list: list, n=10, pos=None, form='表層形'):
+    neko_co_dict = {}
+    for sentence in pos_list:
+        if [d for d in sentence if d.get('表層形') == '猫']:
+            for word in sentence:
+                if word['表層形'] != '猫' and (not pos or word['品詞'] == pos):
+                    neko_co_dict[word[form]] = neko_co_dict.get(word[form], 0) + 1
+
+    sorted_neko_co_dict = dict(sorted(neko_co_dict.items(), key=lambda item: item[1], reverse=True))
+    top_n_list = list(sorted_neko_co_dict.items())[:n]
+    plt.bar([i[0] for i in top_n_list], [i[1] for i in top_n_list])
+    plt.title(f'A37: Top {n} frequent words with "猫"')
+    plt.xlabel('Word')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+# A38 ヒストグラム
+def A38(sorted_frequent_dict: dict):
+    plt.hist(sorted_frequent_dict.values(),
+             bins=30)
+    plt.title("A38: 単語の出現頻度とその数")
+    plt.xlabel('出現頻度')
+    plt.ylabel('種類数')
+    plt.ylim(0, 100)
+    plt.show()
+
+# A39 Zipfの法則
+def A39(sorted_frequent_dict: dict):
+    plt.loglog([i+1 for i in range(len(sorted_frequent_dict))],
+            [i for i in sorted_frequent_dict.values()])
+    plt.title("A39: 単語の出現頻度と順位の両対数")
+    plt.xlabel("出現頻度順位")
+    plt.ylabel("出現頻度")
+    plt.show()
+
+
+if __name__ == '__main__':
+    A30_pos_list = A30(neko_txt_path)
+    print("A30:", '\n', A30_pos_list)
+    print('')
+    print("A31:", '\n', A31(A30_pos_list))
+    print('')
+    print("A32:", '\n', A32(A30_pos_list))
+    print('')
+    print("A33:", '\n', A33(A30_pos_list))
+    print('')
+    print("A34:", '\n', A34(A30_pos_list))
+    print('')
+    A35_sorted_frequent_dict = A35(A30_pos_list)
+    print("A35:", '\n', A35_sorted_frequent_dict)
+    A36(A35_sorted_frequent_dict)
+    A37(A30_pos_list, pos='動詞')
+    A38(A35_sorted_frequent_dict)
+    A39(A35_sorted_frequent_dict)
+
---
+++ C5/Chapter5.py
@@ -0,0 +1,394 @@
+ai_txt_path = 'ai/ai.ja.txt'
+ai_output_path = 'ai/ai.ja.txt.parsed'
+A42_output_txt_path = 'A42.txt'
+A43_output_txt_path = 'A43.txt'
+A45_output_txt_path = 'A45.txt'
+A46_output_txt_path = 'A46.txt'
+A47_output_txt_path = 'A47.txt'
+A48_output_txt_path = 'A48.txt'
+A49_output_txt_path = 'A49.txt'
+
+import ginza
+import spacy
+import json
+import os
+
+nlp = spacy.load("ja_ginza")
+
+
+# with open(ai_txt_path, 'r') as input_txt:
+#     with open(ai_output_path, 'w') as output_txt:
+#
+#         output_list = []
+#         for line in input_txt:
+#             doc = nlp(line)
+#
+#             for sent in doc.sents:
+#                 sentence_dict = {}
+#                 pos_list = []
+#                 dep_list = []
+#
+#                 if sent.__len__() > 1:
+#                     for span in ginza.bunsetu_spans(sent):
+#                         pos_list2 = []
+#
+#                         for token in span:
+#                             token_pos = token.tag_.split('-')
+#                             # (order, surface, base, pos, pos1)
+#                             pos_list2.append([token.i,
+#                                               token.text,
+#                                               token.lemma_,
+#                                               token_pos[0],
+#                                               token_pos[2] if len(token_pos) > 1 and token_pos[1] == "普通名詞" else (token_pos[1] if len(token_pos) > 1 else '-')
+#                                               ])
+#
+#                         pos_list.append(pos_list2)
+#
+#                         for token in span.lefts:
+#                             dep_list.append((ginza.bunsetu_spans(sent).index(ginza.bunsetu_span(token)),
+#                                              ginza.bunsetu_spans(sent).index(span)))
+#
+#                 if pos_list and dep_list:
+#                     sentence_dict['pos'] = pos_list
+#                     sentence_dict['dep'] = dep_list
+#
+#                     output_list.append(sentence_dict)
+#
+#         json.dump(output_list, output_txt, ensure_ascii=False)
+
+# A40 係り受け解析結果の読み込み（形態素）
+class Morph:
+    def __init__(self, surface, base, pos, pos1):
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
+
+    def __repr__(self):
+        return f'(表層形: {self.surface}, 基本形: {self.base}, 品詞: {self.pos}, 品詞細分類1: {self.pos1})'
+
+
+# A41 係り受け解析結果の読み込み（文節・係り受け）
+class Chunk:
+    def __init__(self, morphs, dst, srcs):
+        self.morphs = morphs
+        self.dst = dst
+        self.srcs = srcs
+
+    def __repr__(self):
+        return f'(形態素: {self.morphs}, 係り先: {self.dst}, 係り元: {self.srcs})'
+
+    def check_sent(self, condition: str) -> bool:
+        if any(condition in morph.pos for morph in self.morphs):
+            return True
+
+        return False
+
+    def get_sent(self) -> str:
+        sent = ''
+        for morph in self.morphs:
+            if morph.pos != '補助記号':
+                sent += morph.surface
+
+        return sent
+
+    def get_morphs_sent(self, condition: str) -> list:
+        morph_list = []
+        for morph in self.morphs:
+            if condition in morph.pos:
+                morph_list.append(morph.surface)
+
+        return morph_list
+
+    def get_morphs(self, condition: str) -> list:
+        morph_list = []
+        for morph in self.morphs:
+            if morph.pos == condition:
+                morph_list.append(morph)
+
+        return morph_list
+
+
+def A40(input_path: str) -> list:
+    with open(input_path, 'r') as txt:
+        output_list = []
+        for line in json.load(txt):
+            sent_dict = {}
+            sent_morph_list = []
+            sent_chunk_list = []
+            bunsetu_dep_list = line['dep']
+            current_bunsetu = 0
+
+            for bunsetu in line['pos']:
+                chunk_bunsetu_list = []
+                src_list = []
+
+                for word in bunsetu:
+                    word_morph = Morph(word[1], word[2], word[3], word[4])
+                    sent_morph_list.append(word_morph)
+                    chunk_bunsetu_list.append(word_morph)
+
+                for dep in bunsetu_dep_list:
+                    if current_bunsetu in dep:
+                        if dep.index(current_bunsetu) == 0:
+                            dst = dep[1]
+                        elif dep.index(current_bunsetu) == 1:
+                            src_list.append(dep[0])
+
+                sent_chunk_list.append(Chunk(chunk_bunsetu_list,
+                                             dst if dst else '',
+                                             src_list if src_list else ''))
+                current_bunsetu += 1
+
+            sent_dict['morph'] = sent_morph_list
+            sent_dict['chunk'] = sent_chunk_list
+            output_list.append(sent_dict)
+
+    return output_list
+
+
+# A42 係り元と係り先の文節の表示
+def A42(sent_list: list):
+    with open(A42_output_txt_path, 'w') as output_txt:
+        for sent in sent_list:
+            for bunsetu in sent['chunk']:
+                if sent['chunk'].index(bunsetu) != bunsetu.dst:
+                    line = bunsetu.get_sent() + '\t' + sent['chunk'][bunsetu.dst].get_sent() + '\n'
+                    output_txt.write(line)
+
+
+# A43 名詞を含む文節が動詞を含む文節に係るものを抽出
+def A43(sent_list: list):
+    with open(A43_output_txt_path, 'w') as output_txt:
+        for sent in sent_list:
+            for bunsetu in sent['chunk']:
+                if sent['chunk'].index(bunsetu) != bunsetu.dst:
+                    if bunsetu.check_sent('名詞') and sent['chunk'][bunsetu.dst].check_sent('動詞'):
+                        line = bunsetu.get_sent() + '\t' + sent['chunk'][bunsetu.dst].get_sent() + '\n'
+                        output_txt.write(line)
+
+
+# A44 係り受け木の可視化
+def A44(sentence: list):
+    words = []
+    for bunsetu in sentence:
+        words.append(bunsetu.get_sent())
+    spaces = [True] * len(words)
+    doc = spacy.tokens.Doc(nlp.vocab, words=words, spaces=spaces)
+
+    for i, token in enumerate(doc):
+        if sentence[i].dst == i:
+            token.dep_ = "ROOT"
+        else:
+            token.dep_ = "dep"
+        token.head = doc[sentence[i].dst]
+
+    spacy.displacy.serve(doc, style="dep", auto_select_port=True)
+
+
+# A45 動詞の格パターンの抽出
+# A46 動詞の格フレーム情報の抽出
+def A45(sent_list: list):
+    with open(A45_output_txt_path, 'w') as output_txt1:
+        with open(A46_output_txt_path, 'w') as output_txt2:
+            for sent in sent_list:
+                for bunsetu in sent['chunk']:
+                    verb_bool = False
+
+                    for word in bunsetu.morphs:
+                        if word.pos == '動詞':
+                            predicate = word.base
+                            verb_bool = True
+                            break
+
+                    if verb_bool:
+                        if bunsetu.srcs:
+                            bunsetu_cases = []
+
+                            for idx in bunsetu.srcs:
+                                dep_bunsetu = sent['chunk'][idx]
+                                if dep_bunsetu.check_sent('助詞'):
+                                    bunsetu_cases.append([dep_bunsetu.get_morphs_sent('助詞'), dep_bunsetu.get_sent()])
+
+                            if len(bunsetu_cases) == 0:
+                                break
+
+                            sorted_bunsetu_cases = sorted(bunsetu_cases, key=lambda x: x[0][0])
+
+                            morphs = [bunsetu_case[0] for bunsetu_case in bunsetu_cases]
+                            morphs = [morph for morph_list in morphs for morph in morph_list]
+
+                            line1 = predicate + '\t' + '\t'.join(sorted(morphs)) + '\n'
+                            line2 = predicate + '\t' + '\t'.join(sorted(morphs)) + '\t' + '\t'.join(
+                                [sorted_bunsetu_case[1] for sorted_bunsetu_case in sorted_bunsetu_cases]) + '\n'
+
+                            output_txt1.write(line1)
+                            output_txt2.write(line2)
+                            break
+
+
+# A47 機能動詞構文のマイニング
+def A47(sent_list: list):
+    with open(A47_output_txt_path, 'w') as output_txt:
+        for sent in sent_list:
+            for bunsetu in sent['chunk']:
+                for word1, word2 in zip(bunsetu.morphs, bunsetu.morphs[1:]):
+                    if word1.pos1 == 'サ変可能' and word2.surface == 'を':
+                        base_predicate = word1.surface + word2.surface
+                        verb_bool = False
+                        for word in sent['chunk'][bunsetu.dst].morphs:
+                            if word.pos == '動詞':
+                                predicate = base_predicate + word.base
+                                verb_bool = True
+                                break
+
+                        if verb_bool:
+                            if sent['chunk'][bunsetu.dst].srcs or bunsetu.srcs:
+                                bunsetu_cases = []
+                                for idx in sent['chunk'][bunsetu.dst].srcs + bunsetu.srcs if \
+                                        sent['chunk'][bunsetu.dst].srcs and bunsetu.srcs else \
+                                        sent['chunk'][bunsetu.dst].srcs if sent['chunk'][
+                                            bunsetu.dst].srcs else bunsetu.srcs:
+                                    dep_bunsetu = sent['chunk'][idx]
+                                    if dep_bunsetu.check_sent('助詞'):
+                                        bunsetu_cases.append(
+                                            [dep_bunsetu.get_morphs_sent('助詞'), dep_bunsetu.get_sent()])
+
+                                if len(bunsetu_cases) == 0:
+                                    break
+
+                                sorted_bunsetu_cases = sorted(bunsetu_cases, key=lambda x: x[0][0])
+
+                                morphs = [bunsetu_case[0] for bunsetu_case in bunsetu_cases]
+                                morphs = [morph for morph_list in morphs for morph in morph_list]
+
+                                line = predicate + '\t' + '\t'.join(morphs) + '\t' + '\t'.join(
+                                    [sorted_bunsetu_case[1] for sorted_bunsetu_case in
+                                     sorted_bunsetu_cases]) + '\n'
+
+                                output_txt.write(line)
+                                break
+
+
+# A48 名詞から根へのパスの抽出
+def A48(sent_list: list):
+    with open(A48_output_txt_path, 'w') as output_txt:
+        for sent in sent_list:
+            for bunsetu in sent['chunk']:
+                if bunsetu.check_sent("名詞"):
+                    current_bunsetu = bunsetu
+                    bunsetu_list = []
+
+                    while True:
+                        if current_bunsetu.dst == sent['chunk'].index(current_bunsetu):
+                            bunsetu_list.append(current_bunsetu.get_sent())
+                            break
+                        else:
+                            bunsetu_list.append(current_bunsetu.get_sent())
+                            current_bunsetu = sent['chunk'][current_bunsetu.dst]
+
+                    if len(bunsetu_list) > 1:
+                        line = ' -> '.join(bunsetu_list) + '\n'
+                        output_txt.write(line)
+
+
+# A49 名詞間の係り受けパスの抽出
+def sub_X_Y(bunsetu: Chunk, X_Y='X') -> str:
+    noun_chunks = bunsetu.get_morphs_sent('名詞')
+    print(bunsetu.get_sent())
+    print(noun_chunks)
+
+    if len(noun_chunks) >= 1:
+        start_idx = bunsetu.get_sent().find(noun_chunks[0])
+        end_idx = bunsetu.get_sent().rfind(noun_chunks[-1]) + len(noun_chunks[-1])
+        line = bunsetu.get_sent().replace(
+            bunsetu.get_sent()[start_idx:end_idx],
+            X_Y)
+    else:
+        line = bunsetu.get_sent().replace(noun_chunks[0].surface, X_Y)
+
+    print(line)
+    return line
+
+
+def A49(sent_list: list):
+    with open(A49_output_txt_path, 'w') as output_txt:
+        for sent in sent_list:
+            for idx in range(len(sent['chunk'])):
+                if sent['chunk'][idx].check_sent('名詞'):
+                    pair_idx_list = []
+                    dep_idx_list = []
+                    current_bunsetu = sent['chunk'][idx]
+
+                    for idx2 in range(idx + 1, len(sent['chunk'])):
+                        if sent['chunk'][idx2].check_sent('名詞'):
+                            pair_idx_list.append(idx2)
+
+                    if pair_idx_list:
+                        while True:
+                            if current_bunsetu.dst == sent['chunk'].index(current_bunsetu):
+                                break
+                            else:
+                                dep_idx_list.append(current_bunsetu.dst)
+                                current_bunsetu = sent['chunk'][current_bunsetu.dst]
+
+                        if not dep_idx_list:
+                            break
+
+                        for pair_idx in pair_idx_list:
+                            line = sub_X_Y(sent['chunk'][idx])
+                            line1 = line
+                            line2 = line
+
+                            if pair_idx in dep_idx_list:
+                                for idx3 in dep_idx_list:
+                                    if pair_idx == idx3:
+                                        line1 = line1 + ' -> ' + sub_X_Y(sent['chunk'][idx3], 'Y')
+                                        break
+                                    else:
+                                        line1 = line1 + ' -> ' + sent['chunk'][idx3].get_sent()
+
+                                output_txt.write(line1 + '\n')
+                            else:
+                                pair_dep_idx_list = []
+                                current_pair_bunsetu = sent['chunk'][pair_idx]
+
+                                while True:
+                                    if current_pair_bunsetu.dst == sent['chunk'].index(current_pair_bunsetu):
+                                        break
+                                    else:
+                                        pair_dep_idx_list.append(current_pair_bunsetu.dst)
+                                        current_pair_bunsetu = sent['chunk'][current_pair_bunsetu.dst]
+
+                                for dep_idx in range(len(dep_idx_list) - 1):
+                                    line2 = line2 + ' -> ' + sent['chunk'][dep_idx].get_sent()
+
+                                line3 = sub_X_Y(sent['chunk'][pair_idx], 'Y')
+
+                                for pair_dep_idx in range(len(pair_dep_idx_list) - 1):
+                                    line3 =  line3 + ' -> ' + sent['chunk'][pair_dep_idx].get_sent()
+
+                                line2 = line2 + ' | ' + line3 + ' | ' + sent['chunk'][dep_idx_list[-1]].get_sent()
+
+                                output_txt.write(line2 + '\n')
+
+
+if __name__ == '__main__':
+    A40_sent_list = A40(ai_output_path)
+    print("A40:", '\n')
+    print(A40_sent_list[0]['morph'])
+    print(A40_sent_list[0]['chunk'])
+    A42(A40_sent_list)
+    A43(A40_sent_list)
+    A44(A40_sent_list[0]['chunk'])
+    A45(A40_sent_list)
+    os.system("""cat A45.txt | awk 'BEGIN {FS="\t"} {for(i=2; i<=NF; i++) print $1 " " $i}' | sort | uniq -c | sort -nr""")
+    print("")
+    os.system("""cat A45.txt | awk 'BEGIN {FS="\t"} {if ($1 == "行う") for(i=2; i<=NF; i++) print $1 " " $i}' | sort | uniq -c | sort -nr""")
+    print("")
+    os.system("""cat A45.txt | awk 'BEGIN {FS="\t"} {if ($1 == "なる") for(i=2; i<=NF; i++) print $1 " " $i}' | sort | uniq -c | sort -nr""")
+    print("")
+    os.system("""cat A45.txt | awk 'BEGIN {FS="\t"} {if ($1 == "与える") for(i=2; i<=NF; i++) print $1 " " $i}' | sort | uniq -c | sort -nr""")
+    A47(A40_sent_list)
+    A48(A40_sent_list)
+    A49(A40_sent_list)
